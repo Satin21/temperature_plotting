394ccd8b3d13991b3aa70c90c67182084bd629d2
sathish <rksathish09@gmail.com>
2022-12-15 10:17:30 +0100

    add unit and integration tests

---
+++ tests/test_temperature_plotting.py
@@ -0,0 +1,19 @@
+import pytest
+import temperature_plotting as tpl
+import os
+
+def test_compute_mean():
+    calc = tpl.compute_mean([0, 10, 20])
+    assert calc == 10
+    assert isinstance(calc, float)
+
+    with pytest.raises(TypeError):
+        calc = tpl.compute_mean(["a", "b", "c"])
+
+test_compute_mean()
+
+def test_main():
+    tpl.main()
+    assert os.path.exists("plot_25.png")
+
+test_main()
